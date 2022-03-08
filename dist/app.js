fad7b52927093d1429bdfe56321e6c17851ee2dd
namddph17471 <namddph17471@fpt.edu.vn>
2022-03-08 17:57:41 +0700

    Buoi 1 typescript

---
+++ dist/app.js
@@ -0,0 +1,24 @@
+"use strict";
+// cài typescript: npm i -g typescript
+// -g: global
+// --save : lưu trg 1 project cụ thể
+// --save-dev: ko bắt buộc trg 1 project
+// let a:number = 7;
+// let b:string = "hế lồ";
+// let c:boolean = true;
+// const result = a + b;
+// if (c) {
+//     console.log(c + result);
+// }else{
+//     console.log("akckajcak");
+// }
+const person = {
+    id: 1,
+    name: "Vo Thuy",
+    age: 1254
+};
+const person2 = [
+    { id: 1, name: "Tu", age: 12454, status: true },
+    { id: 2, name: "Truong Huyen", age: 100000, status: true },
+];
+console.log(person2);
