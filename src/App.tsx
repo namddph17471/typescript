d7ebafd89716f0dc6deef80a44982db4abf97691
namddph17471 <namddph17471@fpt.edu.vn>
2022-03-15 20:13:43 +0700

    list & remove products

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,47 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
-import ShowInfo from './ShowInfo'
+import ShowInfo from './components/ShowInfo'
+import type { ProductType } from './types/product'
+import axios from 'axios'
+import { list, remove } from './api/products'
 
 function App() {
-  const [count, setCount] = useState(0)
-type TProduct = {
-  id:number,
-  name:string,
-}
-const [products,setProduct] = useState<TProduct[]>([{id:1,name:'Vô thủy'}])
+
+  const[product,SetProduct] = useState<ProductType[]>([]);
+  useEffect(()=>{
+    const getProducts =  async ()=>{
+      const {data} = await list()
+      SetProduct(data)
+    }
+    getProducts()
+  },[])
+  const removeItem= async (id:number)=>{
+  const {data} =  await remove(id);
+  data && SetProduct(product.filter(item => item.id !== id));
+
+  }
   return (
     <div className="App">
-      < ShowInfo name='Từ'/>
+      <table>
+        <thead>
+          <th>#</th>
+          <th>Name</th>
+          <th></th>
+        </thead>
+        <tbody>
+          {product.map((item, index) => {
+            return <tr key={index}>
+                    <td>{index + 1}</td>
+                    <td>{item.name}</td>
+                    <td>
+                      <button onClick={() => removeItem(item.id)}>Remove</button>
+                    </td>
+                  </tr>
+          })}
+        </tbody>
+      </table>
+      
     </div>
   )
 }
---
+++ src/components/ShowInfo.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import type { ProductType } from '../types/product'
+type ShowInfoProp={
+  info :ProductType
+}
+const ShowInfo = (props:ShowInfoProp) => {
+  return (
+    <div>
+      <h1>
+        {props.info.age}
+      </h1>
+      <h1>
+        {props.info.name}
+      </h1>
+    </div>
+  )
+}
+
+export default ShowInfo
