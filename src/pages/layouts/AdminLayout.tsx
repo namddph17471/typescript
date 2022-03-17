0cb5c950d0b2fd8295dbab1d1b8ca4e65f4eb905
namddph17471 <namddph17471@fpt.edu.vn>
2022-03-18 06:42:50 +0700

    update

--- src/pages/layouts/AdminLayout.tsx
+++ src/pages/layouts/AdminLayout.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import HeaderAdmin from '../components/HeaderAdmin'
-import MenuAdmin from '../components/MenuAdmin'
+import HeaderAdmin from '../../components/HeaderAdmin'
+import MenuAdmin from '../../components/MenuAdmin'
 
 type Props = {}
 
--- src/pages/layouts/WebsiteLayout.tsx
+++ src/pages/layouts/WebsiteLayout.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import Banner from '../components/Banner'
-import Footer from '../components/Footer'
-import Header from '../components/Header'
+import Banner from '../../components/Banner'
+import Footer from '../../components/Footer'
+import Header from '../../components/Header'
 
 type Props = {}
 
