726c15492e83b9b284e114a92458b4cc7d189bb9
namddph17471 <namddph17471@fpt.edu.vn>
2022-03-29 20:32:49 +0700

    create Layout

---
+++ Assignment/src/components/MenuAdmin.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const MenuAdmin = (props: Props) => {
+  return (
+    <div>MenuAdmin</div>
+  )
+}
+
+export default MenuAdmin
---
+++ Assignment/src/pages/New.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const New = (props: Props) => {
+  return (
+    <div>New</div>
+  )
+}
+
+export default New
